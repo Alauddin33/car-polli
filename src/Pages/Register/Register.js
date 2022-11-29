92ead115d6b9f5cd61cedf2184628676a42d53ef
Alauddin33 <shamem33@gmail.com>
2022-11-29 12:24:31 +0600

    token set to local storage

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -70,11 +70,21 @@ const Register = () => {
             })
             .then(res => res.json())
             .then(data => {
-                console.log('saveuser', data);
-                navigate(from, { replace: true });
+                getUserToken(email);
             })
     }
 
+
+    const getUserToken = email => {
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('accessToken', data.accessToken);
+                    navigate(from, { replace: true });
+                }
+            })
+    }
 
 
     return (
